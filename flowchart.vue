59495fccb6742a684129f22e70cb160401ec7056
ULIVZ <472590061@qq.com>
2018-10-03 16:13:31 +0800

    style: tweaks

--- flowchart.vue
+++ flowchart.vue
@@ -20,70 +20,72 @@ export default {
   mounted () {
     const code = this.$el.innerText
     this.$el.removeChild(this.$el.childNodes[0])
-    const svg = flowchart.parse(code)
-    svg.drawSVG(this.id, {
-      'x': 0,
-      'y': 0,
-      'line-width': 2,
-      'line-length': 50,
-      'text-margin': 10,
-      'font-size': 14,
-      'font-color': '#8DA1AC',
-      'line-color': '#8DA1AC',
-      'element-color': 'black',
-      'fill': 'white',
-      'yes-text': 'yes',
-      'no-text': 'no',
-      'arrow-end': 'block',
-      'scale': 1,
-      // style symbol types
-      'symbols': {
-        start: {
-          'class': 'start-element',
-          'font-color': 'white',
-          'fill': '#2F495F',
-          'line-width': '0px'
-        },
-        end: {
-          'class': 'end-element',
-          'font-color': 'white',
-          'fill': '#2F495F',
-          'line-width': '0px'
-        },
-        operation: {
-          'class': 'operation-element',
-          'font-color': 'white',
-          'fill': '#00BC7D',
-          'line-width': '0px',
-          'rx': '5px',
-          'ry': '5px'
-        },
-        inputoutput: {
-          'class': 'inputoutput-element',
-          'font-color': 'white',
-          'fill': '#EB4D5D',
-          'line-width': '0px'
-        },
-        subroutine: {
-          'class': 'subroutine-element',
-          'font-color': 'white',
-          'fill': '#937AC4',
-          'element-color': '#fff',
-          'line-color': 'red'
-        },
-        condition: {
-          'class': 'condition-element',
-          'font-color': 'white',
-          'fill': '#FFB500',
-          'line-width': '0px'
-        },
-        parallel: {
-          'class': 'parallel-element',
-          'font-color': 'white',
-          'fill': '#2F495F',
-          'line-width': '0px'
+    import(/* webpackChunkName: "flowchart" */ 'flowchart.js').then(flowchart => {
+      console.log(code)
+      flowchart = flowchart.default
+      const svg = flowchart.parse(code)
+      svg.drawSVG(this.id, {
+        'x': 0,
+        'y': 0,
+        'line-width': 2,
+        'line-length': 50,
+        'text-margin': 10,
+        'font-size': 14,
+        'font-color': '#8DA1AC',
+        'line-color': '#8DA1AC',
+        'element-color': 'black',
+        'fill': 'white',
+        'yes-text': 'yes',
+        'no-text': 'no',
+        'arrow-end': 'block',
+        'scale': 1,
+        // style symbol types
+        'symbols': {
+          start: {
+            'class': 'start-element',
+            'font-color': 'white',
+            'fill': '#2F495F',
+            'line-width': '0px'
+          },
+          end: {
+            'class': 'end-element',
+            'font-color': 'white',
+            'fill': '#2F495F',
+            'line-width': '0px'
+          },
+          operation: {
+            'class': 'operation-element',
+            'font-color': 'white',
+            'fill': '#00BC7D',
+            'line-width': '0px'
+          },
+          inputoutput: {
+            'class': 'inputoutput-element',
+            'font-color': 'white',
+            'fill': '#EB4D5D',
+            'line-width': '0px'
+          },
+          subroutine: {
+            'class': 'subroutine-element',
+            'font-color': 'white',
+            'fill': '#937AC4',
+            'element-color': '#fff',
+            'line-color': 'red'
+          },
+          condition: {
+            'class': 'condition-element',
+            'font-color': 'white',
+            'fill': '#FFB500',
+            'line-width': '0px'
+          },
+          parallel: {
+            'class': 'parallel-element',
+            'font-color': 'white',
+            'fill': '#2F495F',
+            'line-width': '0px'
+          }
         }
-      }
+      })
     })
   }
 }
@@ -95,4 +97,8 @@ export default {
   text-align center
   & > svg
     max-width 100%
+
+.operation-element, .parallel-element
+  rx 5px
+  ry 5px
 </style>
